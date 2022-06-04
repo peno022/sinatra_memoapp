81b1d8f6d3c1b492d6c64dbb1f0c2bb04464c6c5
mayumonj <>
2022-06-04 18:46:32 +0900

    リファクタ、.rubocop.yaml追加

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-# gem "rails"
-
-gem "sinatra", "~> 2.2"
+gem 'sinatra', '~> 2.2'
 gem 'sinatra-contrib'
+gem 'sinatra-flash'
+gem 'webrick', '~> 1.7'
 
-gem "webrick", "~> 1.7"
-gem "sinatra-flash"
+group :development do
+  gem 'rubocop-fjord', require: false
+end
--- memoapp.rb
+++ memoapp.rb
@@ -6,6 +6,9 @@ require 'sinatra/flash'
 require 'json'
 
 enable :sessions
+
+ERROR_MESSAGE_DELETED_MEMO = '既に削除されたメモです。'
+ERROR_MESSAGE_EMPTY_MEMO = 'タイトル、内容にはテキストを入力してください。'
 class Memo
   attr_accessor :id, :title, :content
 
@@ -16,54 +19,48 @@ class Memo
   end
 
   def self.all
-    memos_hash = ''
-    File.open('data.json') do |file|
-      memos_hash = JSON.parse(file.read)['memos']
+    memo_hash_objects = []
+    File.open('data.json') do |f|
+      memo_hash_objects = JSON.parse(f.read)['memos']
     end
-    memos = []
-    memos_hash.each do |memo_hash|
-      memos.push(Memo.new(memo_hash['id'], memo_hash['title'], memo_hash['content']))
+    memo_hash_objects.map do |memo_hash_object|
+      Memo.new(memo_hash_object['id'], memo_hash_object['title'], memo_hash_object['content'])
     end
-    memos
   end
 
   def self.find_by_id(id)
     all.find { |memo| memo.id == id }
   end
 
-  def add
+  def create
     Memo.write(Memo.all.push(self).map(&:hash))
   end
 
   def delete
-    memos = Memo.all.delete_if { |memo| memo.id == id.to_i }
+    memos = Memo.all.delete_if { |memo| memo.id == @id.to_i }
     Memo.write(memos.map(&:hash))
   end
 
-  def edit
+  def save
     edited_memos = Memo.all.map do |memo|
-      memo.id == id ? self : memo
+      memo.id == @id ? self : memo
     end
     Memo.write(edited_memos.map(&:hash))
   end
 
   def hash
     {
-      "id": id,
-      "title": title,
-      "content": content
+      "id": @id,
+      "title": @title,
+      "content": @content
     }
   end
 
-  def self.write(hash_memos)
-    File.open('data.json', 'w') do |file|
-      JSON.dump({ 'memos' => hash_memos }, file)
+  def self.write(memo_hash_objects)
+    File.open('data.json', 'w') do |f|
+      JSON.dump({ 'memos' => memo_hash_objects }, f)
     end
   end
-end
-
-helpers do
-  include Rack::Utils
 end
 
 get '/' do
@@ -76,7 +73,7 @@ get '/memos' do
   erb :index
 end
 
-get '/new' do
+get '/memos/new' do
   erb :new
 end
 
@@ -85,46 +82,59 @@ get '/memos/edit/:id' do
   erb :edit
 end
 
-get '/:id' do
+get '/memos/:id' do
   @memo = Memo.find_by_id(params['id'].to_i)
   erb :detail
 end
 
 post '/memos' do
   if params['title'].strip.empty? || params['content'].strip.empty?
-    flash[:alert] = 'タイトル、内容にはテキストを入力してください。'
-    redirect '/new'
-  else
-    new_id = Memo.all.empty? ? 0 : Memo.all.map(&:id).max + 1
-    memo = Memo.new(new_id, escape_html(params['title']), escape_html(params['content']))
-    memo.add
-    redirect '/memos'
-    erb :index
+    show_error_message(ERROR_MESSAGE_EMPTY_MEMO)
+    redirect '/memos/new'
+    return
   end
+
+  new_id = Memo.all.empty? ? 0 : Memo.all.map(&:id).max + 1
+  memo = Memo.new(new_id, escape_html(params['title']), escape_html(params['content']))
+  memo.create
+  redirect '/memos'
+  erb :index
 end
 
 patch '/memos/:id' do
-  edited_memo = Memo.find_by_id(params['id'].to_i)
-  if edited_memo
-    edited_memo.title = escape_html(params['title'])
-    edited_memo.content = escape_html(params['content'])
-    edited_memo.edit
+  target_memo = Memo.find_by_id(params['id'].to_i)
+  unless target_memo
+    show_error_message(ERROR_MESSAGE_DELETED_MEMO)
     redirect '/memos'
-    erb :index
-  else
-    flash[:error] = '既に削除されたメモです。'
-    redirect '/memos'
+    return
   end
+
+  if params['title'].strip.empty? || params['content'].strip.empty?
+    show_error_message(ERROR_MESSAGE_EMPTY_MEMO)
+    redirect "/memos/#{params['id']}"
+    return
+  end
+
+  target_memo.title = escape_html(params['title'])
+  target_memo.content = escape_html(params['content'])
+  target_memo.save
+  redirect '/memos'
+  erb :index
 end
 
 delete '/memos/:id' do
-  deleted_memo = Memo.find_by_id(params['id'].to_i)
-  if deleted_memo
-    deleted_memo.delete
+  target_memo = Memo.find_by_id(params['id'].to_i)
+  unless target_memo
+    show_error_message(ERROR_MESSAGE_DELETED_MEMO)
     redirect '/memos'
-    erb :index
-  else
-    flash[:error] = '既に削除されたメモです。'
-    redirect '/memos'
+    return
   end
+
+  target_memo.delete
+  redirect '/memos'
+  erb :index
+end
+
+def show_error_message(message)
+  flash[:error] = message
 end
